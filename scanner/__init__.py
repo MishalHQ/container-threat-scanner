d4eb14ba99cc2006fb6c7e7d171a6b3e136fcbc0
Mohammed Mishal <157570133+MishalHQ@users.noreply.github.com>
2026-02-26 19:10:15 +0530

    Update package init to include HTML report generator

--- scanner/__init__.py
+++ scanner/__init__.py
@@ -1,15 +1,16 @@
 """
-Container Image Threat Scanner
-A layer-aware forensic security analysis tool for container images
+LayerGuard - Layer-Aware Container Image Forensic Threat Scanner
+A professional security analysis tool for container images
 """
 
-__version__ = "1.0.0"
-__author__ = "DevSecOps Team"
+__version__ = "2.0.0"
+__author__ = "LayerGuard Security Team"
 
 from .layer_analysis import LayerAnalyzer
 from .sbom import SBOMGenerator
 from .vulnerability import VulnerabilityScanner
 from .remediation import RemediationEngine
+from .report_generator import HTMLReportGenerator
 from .utils import validate_environment, setup_logging
 
 __all__ = [
@@ -17,6 +18,7 @@ __all__ = [
     'SBOMGenerator',
     'VulnerabilityScanner',
     'RemediationEngine',
+    'HTMLReportGenerator',
     'validate_environment',
     'setup_logging'
 ]
